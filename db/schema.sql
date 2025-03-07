68c6bb5be667247420dab2cc71ce51479fed5f5e
Jpeterson <peterson.jacob13@protonmail.com>
2025-03-06 23:50:57 -0600

    added app logic

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,35 @@
-DROP DATABASE IF EXISTS employee_registry
+DROP DATABASE IF EXISTS employee_registry;
 
-CREATE DATABASE employee_registry
+CREATE DATABASE employee_registry;
 
-CREATE TABLE department{
-    id SERIAL PRIMARY KEY
-    name VARCHAR (30) NOT NULL
-}
+\c employee_registry;
 
-CREATE TABLE role(
-    id SERIAL PRIMARY KEY
-    title VARCHAR (30) NOT NULL
-    salary DECIMAL NOT NULL
-    department INTEGER NOT NULL
-    FOREIGN KEY (department) REFERENCES department(id)
-)
+DROP TABLE IF EXISTS departments;
 
-CREATE TABLE employee(
-    id SERIAL PRIMARY KEY
-    first_name VARCHAR (30) NOT NULL
-    last_name VARCHAR (30) NOT NULL
-    role_id INTEGER NOT NULL
-    manager_id INTEGER
-    FOREIGN KEY (role_id) REFERENCES role(id)
-    FOREIGN key (manager_id) REFERENCES employee(id)
-)
+CREATE TABLE departments(
+    id SERIAL PRIMARY KEY,
+    department_name VARCHAR (30) NOT NULL
+);
+
+DROP TABLE IF EXISTS employee_roles;
+
+CREATE TABLE employee_roles(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR (30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE employees(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    department_id INTEGER,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER REFERENCES employees(id),
+    FOREIGN KEY (role_id) REFERENCES employee_roles(id),
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,42 @@
+-- CREATE TABLE departments(
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR (30) NOT NULL
+-- );
+
+INSERT INTO departments (department_name)
+VALUES
+    ('meatball');
+
+-- CREATE TABLE employee_roles(
+--     id SERIAL PRIMARY KEY,
+--     title VARCHAR (30) NOT NULL,
+--     salary DECIMAL NOT NULL,
+--     department_id INTEGER NOT NULL,
+--     FOREIGN KEY (department_id) REFERENCES departments(id)
+-- );
+
+INSERT INTO employee_roles (title, salary, department_id)
+VALUES
+    ('manager', 12.23, 1),
+    ('meatball roller', 110.25, 1);
+
+-- CREATE TABLE employees(
+--     id SERIAL PRIMARY KEY,
+--     first_name VARCHAR (30) NOT NULL,
+--     last_name VARCHAR (30) NOT NULL,
+--     department_id INTEGER,
+--     role_id INTEGER NOT NULL,
+--     manager_id INTEGER REFERENCES employees(id),
+--     FOREIGN KEY (role_id) REFERENCES role(id),
+--     FOREIGN KEY (department_id) REFERENCES department(id)
+-- );
+
+INSERT INTO employees (first_name, last_name, department_id, role_id, manager_id)
+VALUES
+    ('Sam', 'Johnson', 1, 1, NULL), 
+    ('John', 'Samson', 1, 2, 1);
+
+SELECT e.id, e.first_name, e.last_name, e.manager_id, m.first_name AS manager_first_name, m.last_name AS manager_last_name 
+FROM employees e 
+JOIN employees m ON e.manager_id = m.id
+
